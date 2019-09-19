75b9df1eb9df517da559a259d866d08df2c7360d
Diego Roberto <diego-roberto@hotmail.com>
2019-09-18 22:30:00 -0300

    Criando página inicial de teste HTML, CSS e JS

---
+++ Front-End/js/inicial.js
@@ -0,0 +1,12 @@
+function myFunctionDefault() {
+  document.getElementById("demo").innerHTML = "This is heading";
+}
+
+function trocaBackColor() {
+  document.getElementById("bodyId").classList = "bodyClass2";
+  alert("cor de fundo alterada");
+}
+
+function escreveSaudacao(nome) {
+    document.getElementById("saudacao").innerHTML = "Boa noite, " + nome + "!";
+}
